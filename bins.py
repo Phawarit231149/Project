5777c1bcbe7cbe0ce7096cd6c3ff2049edee90b1
Phawarit231149 <phavarisyatniyom@gmail.com>
2024-11-17 19:54:46 +0700

    Update bins.py

--- bins.py
+++ bins.py
@@ -14,13 +14,13 @@ from skimage.io import imread
 
 dirname = "D:/Project/brain-tumor"
 imagedirname = dirname + "/images"
-image_number= "37"
+image_number= "324" ###You can specify the images number in this part
 my_image=imread(imagedirname + "/Image" + image_number + ".jpg" , as_gray=True )
-my_image = (my_image*255).astype(int) #astype(int) = เปลี่ยนข้อมูลเป็น int
+my_image = (my_image*255).astype(int) 
 
 my_bins=np.histogram(my_image,bins=8)
-new_bins=my_bins[0][1:8]    ### bins is 2d marix so we type [0] to analyze only 1st dimention
-max_bins=np.argmax( new_bins )  ### which bins has most pixels
+new_bins=my_bins[0][1:8]    
+max_bins=np.argmax( new_bins )
 
 plt.figure()
 plt.stairs( new_bins )  
@@ -29,3 +29,8 @@ plt.show()
 tumor=0
 if (max_bins <= 5) and ( new_bins[6] >= 40):
     tumor=1
+
+if ( tumor == 0 ):
+    print ("There is no tumor in this picture")
+else :
+    print ("There is tumor in this picture")
